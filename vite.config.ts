78fae3f788d95fa31b04ff41539a49e8a65e9064
MarkScrivo <84589133+MarkScrivo@users.noreply.github.com>
2024-12-19 16:16:56 -0500

    chore: Clean up service worker code

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,19 +9,6 @@ export default defineConfig({
     host: true,
     hmr: {
       clientPort: 443
-    }
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        serviceWorker: resolve(__dirname, 'src/serviceWorker.ts')
-      },
-      output: {
-        entryFileNames: (assetInfo) => {
-          return assetInfo.name === 'serviceWorker' ? 'serviceWorker.js' : 'assets/[name]-[hash].js'
-        }
-      }
     }
   },
   optimizeDeps: {
